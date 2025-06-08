2b7503a57282c0e2c280ee903cc4652edb48f25a
YuHima03 <yuhima03@gmail.com>
2025-06-09 01:17:02 +0900

    ✨ 同期メソッド CreateRepository の

--- src/Palettes.Domain/Repository/IRepositoryFactory.cs
+++ src/Palettes.Domain/Repository/IRepositoryFactory.cs
@@ -2,7 +2,11 @@
 {
     public interface IRepositoryFactory
     {
-        public IRepository CreateRepository();
+        public IRepository CreateRepository()
+        {
+            return CreateRepositoryAsync(default).AsTask().GetAwaiter().GetResult();
+        }
+
         public ValueTask<IRepository> CreateRepositoryAsync(CancellationToken ct);
     }
 }
