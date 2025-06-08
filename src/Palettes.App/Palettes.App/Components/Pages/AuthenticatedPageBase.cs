29da11f06e2a76d334823e45f6b91a63928f1e42
YuHima03 <yuhima03@gmail.com>
2025-06-08 21:37:14 +0900

    ✨ AuthenticatedPageBase 基底クラス

---
+++ src/Palettes.App/Palettes.App/Components/Pages/AuthenticatedPageBase.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Palettes.App.Services;
+
+namespace Palettes.App.Components.Pages
+{
+    public class AuthenticatedPageBase : ComponentBase
+    {
+#pragma warning disable CS8618
+        [Inject]
+        AuthenticationStateProvider AuthStateProvider { get; set; }
+
+        [Inject]
+        AuthenticatedTraqHttpClientFactory TraqHttpClientFactory { get; set; }
+
+        [Inject]
+        protected Domain.Repository.IRepositoryFactory RepositoryFactory { get; set; }
+#pragma warning restore CS8618
+
+        protected HttpClient? CreateTraqAuthenticatedHttpClient()
+        {
+            return CreateTraqAuthenticatedHttpClientAsync().AsTask().GetAwaiter().GetResult();
+        }
+
+        protected async ValueTask<HttpClient?> CreateTraqAuthenticatedHttpClientAsync()
+        {
+            var authState = await AuthStateProvider.GetAuthenticationStateAsync().ConfigureAwait(false);
+            if (authState.User.Identity is not { IsAuthenticated: true })
+            {
+                return null;
+            }
+            return await TraqHttpClientFactory.CreateClientAsync(AuthStateProvider).ConfigureAwait(false);
+        }
+    }
+}
