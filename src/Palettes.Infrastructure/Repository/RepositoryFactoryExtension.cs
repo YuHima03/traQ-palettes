772572307a5bd08987aa97960e3a4bce6d57ceee
YuHima03 <yuhima03@gmail.com>
2025-06-09 01:52:37 +0900

    ✨ RepositoryFactoryExtension 拡張クラス

---
+++ src/Palettes.Infrastructure/Repository/RepositoryFactoryExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Palettes.Domain.Repository;
+
+namespace Palettes.Infrastructure.Repository
+{
+    public static class RepositoryFactoryExtension
+    {
+        public static IServiceCollection AddRepositoryFactory(this IServiceCollection services, Action<IServiceProvider, DbContextOptionsBuilder> configureOptionsBuilder, ServiceLifetime lifetime = ServiceLifetime.Singleton)
+        {
+            services.AddDbContextFactory<Repository>(configureOptionsBuilder, lifetime);
+            services.TryAdd(new ServiceDescriptor(
+                typeof(IRepositoryFactory),
+                sp => new RepositoryFactory(sp.GetRequiredService<IDbContextFactory<Repository>>()),
+                lifetime));
+            return services;
+        }
+    }
+}
