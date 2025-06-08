20c1411130de2e1d6a03b90912b57a742948a0c9
YuHima03 <yuhima03@gmail.com>
2025-06-08 22:14:38 +0900

    ✨ ORM用の型

---
+++ src/Palettes.Infrastructure/Repository/Models/StampPalette.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Palettes.Infrastructure.Repository.Models
+{
+    [Table("stamp_palettes")]
+    sealed class StampPalette
+    {
+        [Column("id")]
+        public Guid Id { get; set; }
+
+        [Column("user_id")]
+        public Guid UserId { get; set; }
+
+        [Column("is_public")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public bool IsPublic { get; set; }
+
+        [Column("created_at")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime CreatedAt { get; set; }
+
+        [Column("updated_at")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ src/Palettes.Infrastructure/Repository/Models/StampPaletteSubscription.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Palettes.Infrastructure.Repository.Models
+{
+    [Table("stamp_palette_subscriptions")]
+    sealed class StampPaletteSubscription
+    {
+        [Column("id")]
+        public Guid Id { get; set; }
+
+        [Column("user_id")]
+        public Guid UserId { get; set; }
+
+        [Column("palette_id")]
+        public Guid PaletteId { get; set; }
+
+        [Column("copied_palette_id")]
+        public Guid CopiedPaletteId { get; set; }
+
+        [Column("synced_at")]
+        public DateTime SyncedAt { get; set; }
+
+        [Column("created_at")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime CreatedAt { get; set; }
+
+        [Column("updated_at")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
