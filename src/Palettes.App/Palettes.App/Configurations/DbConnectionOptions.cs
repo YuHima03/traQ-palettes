ff7770c11751f74e04969c59eba363998f6e12cb
YuHima03 <yuhima03@gmail.com>
2025-06-09 01:52:58 +0900

    ✨ DbConnectionOptions 設定クラス

---
+++ src/Palettes.App/Palettes.App/Configurations/DbConnectionOptions.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace Palettes.App.Configurations
+{
+    sealed class DbConnectionOptions
+    {
+        [ConfigurationKeyName("NS_MARIADB_USER")]
+        public string? Username { get; set; }
+
+        [ConfigurationKeyName("NS_MARIADB_PASSWORD")]
+        public string? Password { get; set; }
+
+        [ConfigurationKeyName("NS_MARIADB_DATABASE")]
+        public string? DatabaseName { get; set; }
+
+        [ConfigurationKeyName("NS_MARIADB_HOSTNAME")]
+        public string? Host { get; set; }
+
+        [ConfigurationKeyName("NS_MARIADB_PORT")]
+        public uint Port { get; set; }
+
+        public string GetConnectionString() => new MySqlConnectionStringBuilder
+        {
+            UserID = Username,
+            Password = Password,
+            Database = DatabaseName,
+            Server = Host,
+            Port = Port
+        }.ConnectionString;
+    }
+}
