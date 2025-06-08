ed7e863636908efa94cedf9b632a676524d03c3d
YuHima03 <yuhima03@gmail.com>
2025-06-08 21:36:40 +0900

    ✨ IRepositoryFactory

---
+++ src/Palettes.Domain/Repository/IRepositoryFactory.cs
@@ -0,0 +1,8 @@
+﻿namespace Palettes.Domain.Repository
+{
+    public interface IRepositoryFactory
+    {
+        public IRepository CreateRepository();
+        public ValueTask<IRepository> CreateRepositoryAsync(CancellationToken ct);
+    }
+}
