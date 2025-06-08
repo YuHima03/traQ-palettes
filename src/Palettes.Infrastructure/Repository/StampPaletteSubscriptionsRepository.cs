46105cf9bcc330a854184ad98b5ed805e0168706
YuHima03 <yuhima03@gmail.com>
2025-06-09 02:17:36 +0900

    ✨ IStampPaletteSubscriptionsReopsitory の実装用クラス (未実装)

---
+++ src/Palettes.Infrastructure/Repository/StampPaletteSubscriptionsRepository.cs
@@ -0,0 +1,39 @@
+﻿using Palettes.Domain.Models;
+using Palettes.Domain.Repository;
+using Palettes.Infrastructure.Repository.Models;
+
+namespace Palettes.Infrastructure.Repository
+{
+    public sealed partial class Repository : IStampPaletteSubscriptionsRepository
+    {
+        public ValueTask DeleteStampPaletteSubscriptionAsync(Guid id, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<StampPaletteSubscription[]> GetStampPaletteSubscribersAsync(Guid stampPaletteId, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<StampPaletteSubscription> GetStampPaletteSubscriptionAsync(Guid id, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<StampPaletteSubscription[]> GetUserStampPaletteSubscriptionsAsync(Guid userId, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<StampPaletteSubscription> PostStampPaletteSubscriptionAsync(PostStampPaletteSubscriptionRequest request, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<StampPaletteSubscription> UpdateStampPaletteSubscriptionAsync(Guid id, UpdateStampPaletteSubscriptionRequest request, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
