86270b5a8e012a8471605e05b934e96d1128f608
YuHima03 <yuhima03@gmail.com>
2025-06-09 01:19:22 +0900

    ✨ IRepositoryFactory 実装

---
+++ src/Palettes.Infrastructure/Repository/RepositoryFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Palettes.Domain.Repository;
+
+namespace Palettes.Infrastructure.Repository
+{
+    public sealed class RepositoryFactory(IDbContextFactory<Repository> factory) : IRepositoryFactory
+    {
+        public IRepository CreateRepository()
+        {
+            return factory.CreateDbContext();
+        }
+
+        public async ValueTask<IRepository> CreateRepositoryAsync(CancellationToken ct)
+        {
+            return await factory.CreateDbContextAsync(ct);
+        }
+    }
+}
