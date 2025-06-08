ff5b69809ec23cbc6ca5bee99149dad13f7ee4db
YuHima03 <yuhima03@gmail.com>
2025-06-08 22:15:17 +0900

    🚚 StampPaletteRef -> StampPalette

--- src/Palettes.Domain/Repository/IStampPalettesRepository.cs
+++ src/Palettes.Domain/Repository/IStampPalettesRepository.cs
@@ -4,16 +4,16 @@ namespace Palettes.Domain.Repository
 {
     public interface IStampPalettesRepository : IRepositoryBase
     {
-        public ValueTask<StampPaletteRef> GetStampPaletteAsync(Guid id, CancellationToken ct);
+        public ValueTask<StampPalette> GetStampPaletteAsync(Guid id, CancellationToken ct);
 
-        public ValueTask<StampPaletteRef[]> GetPublicStampPalettesAsync(CancellationToken ct);
+        public ValueTask<StampPalette[]> GetPublicStampPalettesAsync(CancellationToken ct);
 
-        public ValueTask<StampPaletteRef[]> GetUserStampPalettesAsync(Guid userId, bool includePrivate, CancellationToken ct);
+        public ValueTask<StampPalette[]> GetUserStampPalettesAsync(Guid userId, bool includePrivate, CancellationToken ct);
 
-        public ValueTask<StampPaletteRef> PostStampPaletteAsync(PostStampPaletteRequest request, CancellationToken ct);
+        public ValueTask<StampPalette> PostStampPaletteAsync(PostStampPaletteRequest request, CancellationToken ct);
 
-        public ValueTask<StampPaletteRef?> TryGetStampPaletteAsync(Guid id, CancellationToken ct);
+        public ValueTask<StampPalette?> TryGetStampPaletteAsync(Guid id, CancellationToken ct);
 
-        public ValueTask<StampPaletteRef> UpdateStampPaletteAsync(Guid id, UpdateStampPaletteRequest request, CancellationToken ct);
+        public ValueTask<StampPalette> UpdateStampPaletteAsync(Guid id, UpdateStampPaletteRequest request, CancellationToken ct);
     }
 }
