30065f55c5526d8880b53e34636e79bebf36cc10
YuHima03 <yuhima03@gmail.com>
2025-06-09 01:14:25 +0900

    ✨ Infrastructure.Repository

---
+++ src/Palettes.Infrastructure/Repository/Repository.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Palettes.Domain.Repository;
+using Palettes.Infrastructure.Repository.Models;
+
+namespace Palettes.Infrastructure.Repository
+{
+    public sealed partial class Repository(DbContextOptions<Repository> options) : DbContext(options), IRepositoryBase
+    {
+        DbSet<RepoStampPalette> StampPalettes { get; set; }
+
+        DbSet<RepoStampPaletteSubscription> StampPaletteSubscriptions { get; set; }
+    }
+}
