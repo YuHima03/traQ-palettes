a293c3df6d59877aedac772a498e815eb2e7ef12
YuHima03 <yuhima03@gmail.com>
2025-06-12 01:41:51 +0900

    ✨ 短縮URLからのリダイレクト

---
+++ src/Palettes.App/Palettes.App/Controllers/ShortenedPathToPalette.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Palettes.App.Controllers
+{
+    [Route("p")]
+    [ApiController]
+    public class ShortenedPathToPalette : ControllerBase
+    {
+        [HttpGet]
+        [Route("{id}")]
+        public LocalRedirectResult RedirectToPalette(string id)
+        {
+            Span<byte> bytes = stackalloc byte[16];
+            if (SimpleBase.Base58.Bitcoin.TryDecode(id, bytes, out var len) && len == bytes.Length)
+            {
+                var guid = new Guid(bytes);
+                return LocalRedirect($"/stamp-palettes/{guid}");
+            }
+            return LocalRedirect("/");
+        }
+    }
+}
