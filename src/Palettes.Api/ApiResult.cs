bd25bc45f3018e6769ecf2b7feeabb0367e357cf
YuHima03 <yuhima03@gmail.com>
2025-06-13 02:05:33 +0900

    ✨ Api.ApiResult 型

---
+++ src/Palettes.Api/ApiResult.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+namespace Palettes.Api
+{
+    public sealed class ApiResult<TResult>
+    {
+        public bool IsSuccessStatusCode
+        {
+            get
+            {
+                int code = (int)StatusCode;
+                return 200 <= code && code < 300;
+            }
+        }
+
+        public required TResult? Result { get; init; }
+
+        public required HttpStatusCode StatusCode { get; init; }
+    }
+}
