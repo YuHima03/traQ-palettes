0673479b0f8c5cd097b0a6308fbed29d60c20ae6
YuHima03 <yuhima03@gmail.com>
2025-06-13 00:13:44 +0900

    ✨ 同期メソッドを追加

--- src/Palettes.App/Palettes.App/Services/AuthenticatedTraqHttpClientFactory.cs
+++ src/Palettes.App/Palettes.App/Services/AuthenticatedTraqHttpClientFactory.cs
@@ -4,10 +4,15 @@ namespace Palettes.App.Services
 {
     sealed class AuthenticatedTraqHttpClientFactory(IHttpClientFactory httpClientFactory)
     {
+        public HttpClient CreateClient(AuthenticationStateProvider authStateProvider)
+        {
+            return CreateClientAsync(authStateProvider).AsTask().GetAwaiter().GetResult();
+        }
+
         public async ValueTask<HttpClient> CreateClientAsync(AuthenticationStateProvider authStateProvider)
         {
             var client = httpClientFactory.CreateClient(Configurations.TraqClientConfigurator.AuthenticatedHttpClientName);
-            var authState = await authStateProvider.GetAuthenticationStateAsync();
+            var authState = await authStateProvider.GetAuthenticationStateAsync().ConfigureAwait(false);
             if (authState.User.Identity is not { IsAuthenticated: true })
             {
                 return client;
