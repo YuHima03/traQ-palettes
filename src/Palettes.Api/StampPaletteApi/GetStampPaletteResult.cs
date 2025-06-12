e2ac7e10094225abbf16a58a9adf9dc2dc70b03c
YuHima03 <yuhima03@gmail.com>
2025-06-13 02:06:14 +0900

    🏗️ IStampPaletteApi を定義 (WIP)

---
+++ src/Palettes.Api/StampPaletteApi/GetStampPaletteResult.cs
@@ -0,0 +1,64 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Palettes.Api.StampPaletteApi
+{
+    public sealed class GetStampPaletteResult
+    {
+        [JsonPropertyName("id")]
+        public required Guid Id { get; init; }
+
+        [JsonPropertyName("creator")]
+        public required User Creator { get; init; }
+
+        [JsonPropertyName("name")]
+        public required string Name { get; init; }
+
+        [JsonPropertyName("description")]
+        public required string Description { get; init; }
+
+        [JsonPropertyName("stamps")]
+        public required Stamp[] Stamps { get; init; }
+
+        [JsonPropertyName("subscribers")]
+        public required Subscription[] Subscriptions { get; init; }
+
+        [JsonPropertyName("createdAt")]
+        public required DateTimeOffset CreatedAt { get; init; }
+
+        [JsonPropertyName("updatedAt")]
+        public required DateTimeOffset UpdatedAt { get; init; }
+
+        public readonly struct Stamp
+        {
+            [JsonPropertyName("id")]
+            public required Guid Id { get; init; }
+
+            [JsonPropertyName("name")]
+            public required string Name { get; init; }
+        }
+
+        public readonly struct Subscription
+        {
+            [JsonPropertyName("user")]
+            public required User User { get; init; }
+
+            [JsonPropertyName("syncedAt")]
+            public required DateTimeOffset SyncedAt { get; init; }
+
+            [JsonPropertyName("createdAt")]
+            public required DateTimeOffset CreatedAt { get; init; }
+        }
+
+        public readonly struct User
+        {
+            [JsonPropertyName("id")]
+            public required Guid Id { get; init; }
+
+            [JsonPropertyName("name")]
+            public required string Name { get; init; }
+
+            [JsonPropertyName("displayName")]
+            public required string DisplayName { get; init; }
+        }
+    }
+}
---
+++ src/Palettes.Api/StampPaletteApi/IStampPaletteApi.cs
@@ -0,0 +1,7 @@
+﻿namespace Palettes.Api.StampPaletteApi
+{
+    public interface IStampPaletteApi
+    {
+        public ValueTask<ApiResult<GetStampPaletteResult>> GetStampPaletteAsync(Guid id, CancellationToken ct = default);
+    }
+}
