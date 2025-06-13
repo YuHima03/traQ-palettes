ae28306bb08536a9d4fe44d7c58dbf3c913bdd39
YuHima03 <yuhima03@gmail.com>
2025-06-13 20:37:00 +0900

    ✨ IUserApi 実装

---
+++ src/Palettes.App/Palettes.App/ApiHandlers/UserApiHandler.cs
@@ -0,0 +1,34 @@
+﻿using Palettes.Api;
+using Palettes.Api.UserApi;
+
+namespace Palettes.App.ApiHandlers
+{
+    sealed partial class ApiHandler : IUserApi
+    {
+        public async ValueTask<ApiResult<GetMeResult>> GetMeAsync(CancellationToken ct = default)
+        {
+            try
+            {
+                var res = await TraqClient.MeApi.GetMeWithHttpInfoAsync(ct);
+                if (res.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    return ApiResult.Ok(new GetMeResult
+                    {
+                        Id = res.Data.Id,
+                        Name = res.Data.Name
+                    });
+                }
+                return ApiResult.Unauthorized<GetMeResult>();
+            }
+            catch (Traq.Client.ApiException e) when (e.ErrorCode == StatusCodes.Status401Unauthorized)
+            {
+                return ApiResult.Unauthorized<GetMeResult>();
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e, "Error while getting user info");
+                return ApiResult.InternalServerError<GetMeResult>();
+            }
+        }
+    }
+}
