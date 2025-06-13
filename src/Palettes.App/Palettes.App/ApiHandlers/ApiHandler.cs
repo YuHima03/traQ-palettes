0a74b1ef6113c99c817f6a65d29d6f7c5f18acbc
YuHima03 <yuhima03@gmail.com>
2025-06-13 18:10:38 +0900

    ✨ IApiClient のサーバー側実装

---
+++ src/Palettes.App/Palettes.App/ApiHandlers/ApiHandler.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace Palettes.App.ApiHandlers
+{
+    sealed partial class ApiHandler(
+        IMemoryCache cache,
+        ILogger<ApiHandler> logger,
+        Domain.Repository.IRepositoryFactory repositoryFactory,
+        Traq.ITraqApiClient traqClient
+        ) : Api.IApiClient
+    {
+        readonly IMemoryCache Cache = cache;
+        readonly ILogger<ApiHandler> Logger = logger;
+        readonly Domain.Repository.IRepositoryFactory RepositoryFactory = repositoryFactory;
+        readonly Traq.ITraqApiClient TraqClient = traqClient;
+
+        public void Dispose() { }
+
+        public ValueTask DisposeAsync()
+        {
+            Dispose();
+            return ValueTask.CompletedTask;
+        }
+    }
+}
