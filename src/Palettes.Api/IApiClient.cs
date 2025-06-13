1563bf701ade8da5641addcdfe3f0f40a7228b36
YuHima03 <yuhima03@gmail.com>
2025-06-13 18:10:06 +0900

    ✨ IApiClient, IApiClientFactory

---
+++ src/Palettes.Api/IApiClient.cs
@@ -0,0 +1,9 @@
+﻿using Palettes.Api.StampPaletteApi;
+
+namespace Palettes.Api
+{
+    public interface IApiClient :
+        IAsyncDisposable,
+        IDisposable,
+        IStampPaletteApi;
+}
---
+++ src/Palettes.Api/IApiClientFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace Palettes.Api
+{
+    public interface IApiClientFactory
+    {
+        public IApiClient CreateApiClient()
+        {
+            return CreateApiClientAsync(default).AsTask().GetAwaiter().GetResult();
+        }
+
+        public ValueTask<IApiClient> CreateApiClientAsync(CancellationToken ct = default);
+    }
+}
