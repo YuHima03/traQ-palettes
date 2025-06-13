8245022ba4614599486d0032fd238ae390b2e42c
YuHima03 <yuhima03@gmail.com>
2025-06-13 18:06:26 +0900

    ✨ List から ConcurrentDictionary に変換

--- src/Palettes.Utils/Caching/Traq/StampApiExtension.cs
+++ src/Palettes.Utils/Caching/Traq/StampApiExtension.cs
@@ -28,7 +28,7 @@ namespace Palettes.Utils.Caching.Traq
             {
                 return await api.GetCachedStampAsync(cache, id, ct);
             }
-            var s = (await api.GetCachedStampsAsync(cache, null, ct)).First(s => s.Name == name); // GetCachedStampsAsync method updates all caches related to stamps.
+            var (_, s) = (await api.GetCachedStampsAsync(cache, null, ct)).First(s => s.Value.Name == name); // GetCachedStampsAsync method updates all caches related to stamps.
             return new(s.Id, s.Name, s.CreatorId, s.CreatedAt, s.UpdatedAt, s.FileId, s.IsUnicode);
         }
 
@@ -43,7 +43,7 @@ namespace Palettes.Utils.Caching.Traq
                 return name;
             }
             var stamps = await api.GetCachedStampsAsync(cache, null, ct);
-            foreach (var s in stamps)
+            foreach (var (_, s) in stamps)
             {
                 if (s.Id == id)
                 {
@@ -54,15 +54,15 @@ namespace Palettes.Utils.Caching.Traq
             return name ?? throw new Exception("Stamp not found.");
         }
 
-        public static async ValueTask<List<StampWithThumbnail>> GetCachedStampsAsync(this IStampApiAsync api, IMemoryCache cache, string? type, CancellationToken ct = default)
+        public static async ValueTask<ConcurrentDictionary<Guid, StampWithThumbnail>> GetCachedStampsAsync(this IStampApiAsync api, IMemoryCache cache, string? type, CancellationToken ct = default)
         {
-            if (cache.TryGetValue<string?, List<StampWithThumbnail>>(CacheSections.AllStamps, type, out var stamps) && stamps is not null)
+            if (cache.TryGetValue<string?, ConcurrentDictionary<Guid, StampWithThumbnail>>(CacheSections.AllStamps, type, out var stamps) && stamps is not null)
             {
                 return stamps;
             }
-            stamps = await api.GetStampsAsync(null, type, ct);
+            stamps = new((await api.GetStampsAsync(null, type, ct)).Select(s => KeyValuePair.Create(s.Id, s)));
             cache.Set(CacheSections.AllStamps, type, stamps, DefaultExpiration);
-            foreach (var s in stamps)
+            foreach (var (_, s) in stamps)
             {
                 Stamp stampAbstraction = new(s.Id, s.Name, s.CreatorId, s.CreatedAt, s.UpdatedAt, s.FileId, s.IsUnicode);
                 cache.Set(CacheSections.Stamp, s.Id, stampAbstraction, DefaultExpiration);
