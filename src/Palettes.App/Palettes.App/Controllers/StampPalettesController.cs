59fe5b6a14a3307286b65c3a3c16b84996bce225
YuHima03 <yuhima03@gmail.com>
2025-06-13 19:03:06 +0900

    ✨ api/stamp-palettes/:id 実装

---
+++ src/Palettes.App/Palettes.App/Controllers/StampPalettesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Palettes.Utils.Authentication.Claims;
+
+namespace Palettes.App.Controllers
+{
+    [Route("api/stamp-palettes")]
+    [ApiController]
+    public class StampPalettesController(
+        Api.IApiClientFactory apiClientFactory,
+        ILogger<StampPalettesController> logger
+        ) : ControllerBase
+    {
+        [HttpGet]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> GetStampPaletteAsync(Guid id)
+        {
+            try
+            {
+                var user = User.ToTraqUserInfo();
+                if (user is null)
+                {
+                    return Unauthorized();
+                }
+                var handler = await apiClientFactory.CreateApiClientAsync(HttpContext.RequestAborted);
+                var result = await handler.GetStampPaletteAsync(id, HttpContext.RequestAborted);
+                return result.StatusCode switch
+                {
+                    System.Net.HttpStatusCode.OK => Ok(result.Result),
+                    System.Net.HttpStatusCode.NotFound => NotFound(),
+                    _ => StatusCode((int)result.StatusCode),
+                };
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, "Error while getting stamp palette {Id}", id);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
