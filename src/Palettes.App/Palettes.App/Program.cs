29e8ae4e18e3e63b75d5ebe57ddf4511c5f7be83
YuHima03 <yuhima03@gmail.com>
2025-06-15 13:04:35 +0900

    ✨ Wasm だけでレンダリング

--- src/Palettes.App/Palettes.App/Program.cs
+++ src/Palettes.App/Palettes.App/Program.cs
@@ -20,7 +20,6 @@ namespace Palettes.App
 
             // Add services to the container.
             builder.Services.AddRazorComponents()
-                .AddInteractiveServerComponents()
                 .AddInteractiveWebAssemblyComponents()
                 .AddAuthenticationStateSerialization();
 
@@ -85,7 +84,6 @@ namespace Palettes.App
 
             app.MapStaticAssets();
             app.MapRazorComponents<Components.App>()
-                .AddInteractiveServerRenderMode()
                 .AddInteractiveWebAssemblyRenderMode()
                 .AddAdditionalAssemblies(typeof(Client._Imports).Assembly);
 
